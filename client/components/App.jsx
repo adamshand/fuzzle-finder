fc1c6615741ec0d6d9abbc5715bbceb9f6db359f
Adam Shand <130651+adamshand@users.noreply.github.com>
2022-07-05 19:33:17 +1200

    sort select now only shows on / and /tag/*

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,12 @@
 import React from 'react'
 import { Routes, Route, NavLink } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { changeOrder } from '../reducers/sortSlice'
 
 import ByTag from './ByTag.jsx'
 import ByGroup from './ByGroup.jsx'
 import Photo from './Photo.jsx'
+import Sort from './Sort.jsx'
 
 function App() {
-  const { order } = useSelector((state) => state.sort)
-  const dispatch = useDispatch()
-
-  function handleChange(e) {
-    e.preventDefault()
-    dispatch(changeOrder(e.target.value))
-  }
-
   return (
     <main>
       <header>
@@ -27,15 +18,7 @@ function App() {
           <NavLink to="/group/name">names</NavLink> {' ♡ '}
           <NavLink to="/group/species">species</NavLink> {' ♡ '}
           <NavLink to="/group/with">contains</NavLink>
-          <div className="sort">
-            {/* <label htmlFor="sort">Sort by</label> */}
-            <select name="sort" defaultValue={order} onChange={handleChange}>
-              <option value="random">sort by: random ✨</option>
-              <option value="views">sort by: views ⬇️</option>
-              <option value="date">sort by: date ⬆️</option>
-              <option value="rdate">sort by: date ⬇️</option>
-            </select>
-          </div>
+          <Sort />
         </nav>
       </header>
       <Routes>
---
+++ client/components/Sort.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { changeOrder } from '../reducers/sortSlice'
+
+function Sort() {
+  const { pathname } = useLocation()
+  const { order } = useSelector((state) => state.sort)
+  const dispatch = useDispatch()
+
+  console.log(`path:`, pathname)
+
+  function handleChange(e) {
+    e.preventDefault()
+    dispatch(changeOrder(e.target.value))
+  }
+
+  return (
+    <div className="sort">
+      {['', 'tag'].includes(pathname.split('/')[1]) && (
+        <select name="sort" defaultValue={order} onChange={handleChange}>
+          <option value="random">✨🔀</option>
+          <option value="views">🔥⬇️</option>
+          <option value="rdate">📅 ⬆️</option>
+          <option value="date">📅 ⬇️</option>
+        </select>
+      )}
+    </div>
+  )
+}
+
+export default Sort
